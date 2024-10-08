e12cf54f63805b9c63429ad442f9d552d8108578
rayhan889 <raydev1604@gmail.com>
2024-10-08 18:26:11 +0700

    add progressbar and timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import { CiBoxList } from "react-icons/ci";
 
+const ProgressBar = ({ completed }: { completed: number }) => {
+  return (
+    <div className="h-[5px] w-full bg-blue-100 rounded-full">
+      <div
+        style={{ width: `${completed}%` }}
+        className={`h-full bg-blue-700 rounded-full text-right`}
+      ></div>
+    </div>
+  );
+};
+
 const QuestionPage = () => {
   const options = [
     {
@@ -17,9 +28,25 @@ const QuestionPage = () => {
   ];
 
   return (
-    <main className="h-screen w-full bg-white flex items-center justify-center">
+    <main className="h-screen w-full bg-white flex flex-col items-center justify-center">
       {/* Background gradient */}
       <div className="absolute h-60 top-0 left-0 w-full bg-gradient-to-b from-blue-500/20 to-white" />
+
+      {/* Progress bar */}
+      <div className="max-w-[38rem] w-full mb-14 flex flex-col gap-y-2 z-50">
+        <div className="w-full flex items-center justify-between">
+          <span className="text-xs font-medium text-slate-950">
+            1 of 10 Questions
+          </span>
+          <span className="text-xs text-slate-950">Nearly there😱</span>
+        </div>
+        <ProgressBar completed={50} />
+      </div>
+
+      {/* Timer */}
+      <div className="text-slate-950 text-base font-medium mb-10">
+        ⏱️ 00:01:30
+      </div>
 
       <div className="max-w-[38rem] h-auto flex flex-col gap-y-6 z-50">
         {/* Button for question list */}
