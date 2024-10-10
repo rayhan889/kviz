e07342539c3e27f8d98992c9dbca4413dc70f44a
rayhan889 <raydev1604@gmail.com>
2024-10-10 15:05:24 +0700

    tidy up

--- src/components/QuestionPage.tsx
+++ src/components/QuestionPage.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
-import { CiBoxList } from "react-icons/ci";
+
 import ProgressBar from "./ui/Progressbar";
-import QuestionListModal from "./ui/QuestionListModal";
 import CountDown from "./ui/CountDown";
+import QuestionBox from "./ui/QuestionBox";
 
 export interface Question {
   category: string;
@@ -18,6 +18,7 @@ const API_URL =
   "https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=boolean";
 
 const API_CATCH_TIME = 5 * 60 * 1000;
+const COUNTDOWN_DURATION_MS = 90000;
 
 function decodeHTMLEntities(text: string) {
   const textArea = document.createElement("textarea");
@@ -28,7 +29,6 @@ function decodeHTMLEntities(text: string) {
 export default function QuestionPage() {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const [questions, setCurrentQuestions] = useState<Question[]>([]);
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [countRightAnswers, setCountRightAnswers] = useState<number>(0);
   const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
 
@@ -74,17 +74,6 @@ export default function QuestionPage() {
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error</div>;
 
-  const options = [
-    {
-      name: "Yes",
-      value: "True",
-    },
-    {
-      name: "No",
-      value: "False",
-    },
-  ];
-
   return (
     <main className="h-screen w-full bg-white flex flex-col items-center justify-center">
       {/* Background gradient */}
@@ -112,52 +101,16 @@ export default function QuestionPage() {
           </div>
 
           {/* Timer */}
-          <CountDown countdown_duration_ms={90000} />
+          <CountDown countdown_duration_ms={COUNTDOWN_DURATION_MS} />
 
-          <div className="max-w-[30rem] w-full h-auto flex flex-col gap-y-6 z-50 lg:max-w-[38rem]">
-            {/* Button for question list */}
-            <button
-              onClick={() => setIsModalOpen(true)}
-              className="w-[10rem] flex items-center gap-x-3 text-blue-700 rounded bg-blue-50 px-3 py-2 text-sm border border-blue-700 shadow-md"
-            >
-              <CiBoxList className="w-6 h-6" />
-              Question List
-            </button>
-
-            <QuestionListModal
-              isOpen={isModalOpen}
-              onClose={() => setIsModalOpen(false)}
-              questions={questions}
-            />
-
-            {/* Question */}
-            <div className="flex justify-start gap-x-2">
-              <span>{currentIndex + 1}. </span>
-              <p className="text-base text-slate-700 line-clamp-3">
-                {currentQuestion?.question}
-              </p>
-            </div>
-
-            {/* Options */}
-            <div className="flex flex-col gap-y-3">
-              {options.map((opt, idx) => (
-                <button
-                  key={idx}
-                  value={opt.value}
-                  className={`w-full rounded-lg p-6 text-slate-950 text-base border text-start ${
-                    selectedAnswer === opt.value
-                      ? opt.value === currentQuestion.correct_answer
-                        ? "bg-emerald-50 border-emerald-500"
-                        : "bg-red-50 border-red-300"
-                      : "border-slate-300"
-                  }`}
-                  onClick={() => handleCheckAnswer(opt.value)}
-                >
-                  {opt.name}
-                </button>
-              ))}
-            </div>
-          </div>
+          {/* Question Box */}
+          <QuestionBox
+            currentIndex={currentIndex}
+            currentQuestion={currentQuestion}
+            questions={questions}
+            handleCheckAnswer={handleCheckAnswer}
+            selectedAnswer={selectedAnswer}
+          />
         </>
       )}
     </main>
---
+++ src/components/ui/QuestionBox.tsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import { CiBoxList } from "react-icons/ci";
+import QuestionListModal from "./QuestionListModal";
+import { Question } from "../QuestionPage";
+
+interface QuestionBoxProps {
+  questions: Question[];
+  currentQuestion: Question;
+  currentIndex: number;
+  handleCheckAnswer: (ans: string) => void;
+  selectedAnswer: string | null;
+}
+
+const QuestionBox: React.FC<QuestionBoxProps> = ({
+  currentIndex,
+  currentQuestion,
+  handleCheckAnswer,
+  questions,
+  selectedAnswer,
+}: QuestionBoxProps) => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const options = [
+    {
+      name: "Yes",
+      value: "True",
+    },
+    {
+      name: "No",
+      value: "False",
+    },
+  ];
+
+  return (
+    <div className="max-w-[30rem] w-full h-auto flex flex-col gap-y-6 z-50 lg:max-w-[38rem]">
+      {/* Button for question list */}
+      <button
+        onClick={() => setIsModalOpen(true)}
+        className="w-[10rem] flex items-center gap-x-3 text-blue-700 rounded bg-blue-50 px-3 py-2 text-sm border border-blue-700 shadow-md"
+      >
+        <CiBoxList className="w-6 h-6" />
+        Question List
+      </button>
+
+      <QuestionListModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        questions={questions}
+      />
+
+      {/* Question */}
+      <div className="flex justify-start gap-x-2">
+        <span>{currentIndex + 1}. </span>
+        <p className="text-base text-slate-700 line-clamp-3">
+          {currentQuestion?.question}
+        </p>
+      </div>
+
+      {/* Options */}
+      <div className="flex flex-col gap-y-3">
+        {options.map((opt, idx) => (
+          <button
+            key={idx}
+            value={opt.value}
+            className={`w-full rounded-lg p-6 text-slate-950 text-base border text-start ${
+              selectedAnswer === opt.value
+                ? opt.value === currentQuestion.correct_answer
+                  ? "bg-emerald-50 border-emerald-500"
+                  : "bg-red-50 border-red-300"
+                : "border-slate-300"
+            }`}
+            onClick={() => handleCheckAnswer(opt.value)}
+          >
+            {opt.name}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default QuestionBox;
